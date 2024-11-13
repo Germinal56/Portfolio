4b445b932dae37bb2d5794ecbb3297527c3a25d2
Germinal56 <stockhudson@mail.com>
2024-11-13 13:04:05 +0100

    reedited export

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,11 +1,17 @@
 import { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
-
+/*
 const nextConfig: NextConfig = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true", // Enable the analyzer only when ANALYZE=true
 })({
   // Other Next.js configurations can go here
   reactStrictMode: true, // Example configuration
 });
+*/
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true, // Example configuration
+  output: 'export',
+};
 
 export default nextConfig;
